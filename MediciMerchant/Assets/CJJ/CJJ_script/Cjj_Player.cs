64f9a8042e5e76ee022e921e0b363dcd46d36672
evanDev <inbear49@gmail.com>
2021-09-28 19:04:55 +0900

    1차 병합

--- MediciMerchant/Assets/CJJ/CJJ_script/Cjj_Player.cs
+++ MediciMerchant/Assets/CJJ/CJJ_script/Cjj_Player.cs
@@ -1,6 +1,8 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Cjj_Player : MonoBehaviour
 {
@@ -21,22 +23,21 @@ public class Cjj_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    int layer = 1 << LayerMask.NameToLayer("Enemy");
+        //    RaycastHit hitInfo;
+        //    if (Physics.Raycast(ray, out hitInfo, float.MaxValue, layer))
+        //    {
+        //        Cjj_Enemy enemy = hitInfo.transform.GetComponent<Cjj_Enemy>();
+        //        enemy.DoDamage(1);
+        //    }
+        //}
 
-        if (Input.GetButtonDown("Fire1"))
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            int layer = 1 << LayerMask.NameToLayer("Enemy");
-            RaycastHit hitInfo;
-            if (Physics.Raycast(ray, out hitInfo, float.MaxValue, layer))
-            {
-                Cjj_Enemy enemy = hitInfo.transform.GetComponent<Cjj_Enemy>();
-                enemy.DoDamage(1);
-            }
-        }
-
-        /*Touch touch = Input.GetTouch(0);
-        if(touch.phase == TouchPhase.Began) 
-        { 
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
             int layer = 1 << LayerMask.NameToLayer("Enemy");
             RaycastHit hitInfo;
@@ -44,15 +45,21 @@ public class Cjj_Player : MonoBehaviour
             {
                 print("아아아아아아");
             }
-        }*/
+        }
 
     }
     public void EnemyEvent()
     {
+        float x = Random.Range(-2, 2);
+        float z = Random.Range(1, 2);
+        float y = transform.position.y;
+
+        Vector3 origin = new Vector3(x, y, z);
         if (enemynumber < 1)
         {
             GameObject enemy = Instantiate(Enemy);
-            enemy.transform.position = transform.position + transform.forward * 3;
+            //enemy.transform.position = transform.position + transform.forward * 3;
+            enemy.transform.position = transform.position + origin;
             enemynumber++;
         }
     }
--- MediciMerchant/Assets/CJJ/CJJ_script/Cjj_SpawnManager.cs
+++ MediciMerchant/Assets/CJJ/CJJ_script/Cjj_SpawnManager.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Cjj_SpawnManager : MonoBehaviour
 {
     public GameObject EnemySpawn;
     public float currenttime = 0;
+
+    TMP_Text ttt;//test
+
     // Start is called before the first frame update
     void Start()
     {
-
+        ttt = GameObject.Find("CHECK").GetComponent<TMP_Text>(); //test
     }
 
     // Update is called once per frame
     void Update()
     {
         currenttime = currenttime + Time.deltaTime;
-        if (currenttime >= 30)
+        if (currenttime >= 5)
         {
             Destroy(gameObject);
         }
@@ -27,15 +31,18 @@ public class Cjj_SpawnManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("MainCamera"))
         {
-            Destroy(gameObject);
+            
+            ttt.text = "Collusion";//test
+            Destroy(gameObject,2);
             Cjj_Player.instance.EnemyEvent();
         }
     }
-
+    
     private void OnDestroy()
     {
         Cjj_CloudSpawnManager.instance.spawnnumber--;
+        ttt.text = "";//test
     }
 }
