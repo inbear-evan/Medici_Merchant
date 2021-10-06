9d44b52cf2eb737bde1959a57c287225c1c14b10
evanDev <inbear49@gmail.com>
2021-10-06 14:48:55 +0900

    log

--- MediciMerchant/Assets/YHC/YH_Scripts/YH_StoreInteraction.cs
+++ MediciMerchant/Assets/YHC/YH_Scripts/YH_StoreInteraction.cs
@@ -18,113 +18,102 @@ public class YH_StoreInteraction : MonoBehaviour
     public int storeIndex = -1;
     void FixedUpdate()
     {
-        bool storeOpenChk = false;
-        for (int i = 0; i < 5; i++)
-        {
+        for (int i = 0; i < 5; i++){
             if (StoreUI[i].gameObject.activeSelf)
-            {
-                storeOpenChk = true;
                 break;
-            }
         }
-        
-
-        if (!storeOpenChk)
+        //if (Input.GetButtonDown("Fire1"))
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
         {
-            //if (Input.GetButtonDown("Fire1"))
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, 10))
             {
-
-                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                RaycastHit hitInfo;
-
-                if (Physics.Raycast(ray, out hitInfo, 10))
+                //storeIndex = -1;
+                if (hitInfo.collider != null)
                 {
-                    //storeIndex = -1;
-                    if (hitInfo.collider != null)
+                    if (hitInfo.collider.gameObject.name == "Store_Naples")
                     {
-                        if (hitInfo.collider.gameObject.name == "Store_Naples")
-                        {
-                            StoreUI[0].gameObject.SetActive(true);
-                            StoreUI[1].gameObject.SetActive(false);
-                            StoreUI[2].gameObject.SetActive(false);
-                            StoreUI[3].gameObject.SetActive(false);
-                            StoreUI[4].gameObject.SetActive(false);
-                            storeIndex = 0;
-                            int rnd = Random.Range(0, 20);
-                            int rnd1 = Random.Range(0, 20);
-                            int rnd2 = Random.Range(0, 20);
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
-                        }
-                        else if (hitInfo.collider.gameObject.name == "Store_Rome")
-                        {
-                            StoreUI[0].gameObject.SetActive(false);
-                            StoreUI[1].gameObject.SetActive(true);
-                            StoreUI[2].gameObject.SetActive(false);
-                            StoreUI[3].gameObject.SetActive(false);
-                            StoreUI[4].gameObject.SetActive(false);
-                            storeIndex = 1;
-                            int rnd = Random.Range(0, 20);
-                            int rnd1 = Random.Range(0, 20);
-                            int rnd2 = Random.Range(0, 20);
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
-                        }
-                        else if (hitInfo.collider.gameObject.name == "Store_Venice")
-                        {
-                            StoreUI[0].gameObject.SetActive(false);
-                            StoreUI[1].gameObject.SetActive(false);
-                            StoreUI[2].gameObject.SetActive(true);
-                            StoreUI[3].gameObject.SetActive(false);
-                            StoreUI[4].gameObject.SetActive(false);
-                            storeIndex = 2;
-                            int rnd = Random.Range(0, 20);
-                            int rnd1 = Random.Range(0, 20);
-                            int rnd2 = Random.Range(0, 20);
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
-                        }
-                        else if (hitInfo.collider.gameObject.name == "Store_Milan")
-                        {
-                            StoreUI[0].gameObject.SetActive(false);
-                            StoreUI[1].gameObject.SetActive(false);
-                            StoreUI[2].gameObject.SetActive(false);
-                            StoreUI[3].gameObject.SetActive(true);
-                            StoreUI[4].gameObject.SetActive(false);
-                            storeIndex = 3;
-                            int rnd = Random.Range(0, 20);
-                            int rnd1 = Random.Range(0, 20);
-                            int rnd2 = Random.Range(0, 20);
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
-                        }
-                        else if (hitInfo.collider.gameObject.name == "Store_Florence")
-                        {
-                            StoreUI[0].gameObject.SetActive(false);
-                            StoreUI[1].gameObject.SetActive(false);
-                            StoreUI[2].gameObject.SetActive(false);
-                            StoreUI[3].gameObject.SetActive(false);
-                            StoreUI[4].gameObject.SetActive(true);
-                            storeIndex = 4;
-                            int rnd = Random.Range(0, 20);
-                            int rnd1 = Random.Range(0, 20);
-                            int rnd2 = Random.Range(0, 20);
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
-                            YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
-                        }
-
+                        StoreUI[0].gameObject.SetActive(true);
+                        StoreUI[1].gameObject.SetActive(false);
+                        StoreUI[2].gameObject.SetActive(false);
+                        StoreUI[3].gameObject.SetActive(false);
+                        StoreUI[4].gameObject.SetActive(false);
+                        storeIndex = 0;
+                        int rnd = Random.Range(0, 20);
+                        int rnd1 = Random.Range(0, 20);
+                        int rnd2 = Random.Range(0, 20);
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
                     }
-                }
+                    else if (hitInfo.collider.gameObject.name == "Store_Rome")
+                    {
+                        StoreUI[0].gameObject.SetActive(false);
+                        StoreUI[1].gameObject.SetActive(true);
+                        StoreUI[2].gameObject.SetActive(false);
+                        StoreUI[3].gameObject.SetActive(false);
+                        StoreUI[4].gameObject.SetActive(false);
+                        storeIndex = 1;
+                        int rnd = Random.Range(0, 20);
+                        int rnd1 = Random.Range(0, 20);
+                        int rnd2 = Random.Range(0, 20);
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
+                    }
+                    else if (hitInfo.collider.gameObject.name == "Store_Venice")
+                    {
+                        StoreUI[0].gameObject.SetActive(false);
+                        StoreUI[1].gameObject.SetActive(false);
+                        StoreUI[2].gameObject.SetActive(true);
+                        StoreUI[3].gameObject.SetActive(false);
+                        StoreUI[4].gameObject.SetActive(false);
+                        storeIndex = 2;
+                        int rnd = Random.Range(0, 20);
+                        int rnd1 = Random.Range(0, 20);
+                        int rnd2 = Random.Range(0, 20);
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
+                    }
+                    else if (hitInfo.collider.gameObject.name == "Store_Milan")
+                    {
+                        StoreUI[0].gameObject.SetActive(false);
+                        StoreUI[1].gameObject.SetActive(false);
+                        StoreUI[2].gameObject.SetActive(false);
+                        StoreUI[3].gameObject.SetActive(true);
+                        StoreUI[4].gameObject.SetActive(false);
+                        storeIndex = 3;
+                        int rnd = Random.Range(0, 20);
+                        int rnd1 = Random.Range(0, 20);
+                        int rnd2 = Random.Range(0, 20);
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
+                    }
+                    else if (hitInfo.collider.gameObject.name == "Store_Florence")
+                    {
+                        StoreUI[0].gameObject.SetActive(false);
+                        StoreUI[1].gameObject.SetActive(false);
+                        StoreUI[2].gameObject.SetActive(false);
+                        StoreUI[3].gameObject.SetActive(false);
+                        StoreUI[4].gameObject.SetActive(true);
+                        storeIndex = 4;
+                        int rnd = Random.Range(0, 20);
+                        int rnd1 = Random.Range(0, 20);
+                        int rnd2 = Random.Range(0, 20);
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[0].ItemQuantity = rnd;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[1].ItemQuantity = rnd1;
+                        YH_ShopManager.instance.stores[storeIndex].InStoreItems[2].ItemQuantity = rnd2;
+                    }
 
+                }
             }
         }
+
     }
 }
